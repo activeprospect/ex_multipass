e96911d14bbaeb7a541cf35b0a21455d33e563ed
Ivy Rogatko <IRog@users.noreply.github.com>
2018-02-16 14:40:05 -0800

    Property test and working encrypt/decryption (#1)
    * Property test and working encrypt/decryption as per our multipass use

--- lib/multipass_ex.ex
+++ lib/multipass_ex.ex
@@ -1,48 +1,55 @@
 defmodule MultipassEx do
   @moduledoc """
-  Documentation for MultipassEx.
+  Mimicking ruby multipass default encryption of aes_cbc128
   """
   @aes128_block_size 16
-  @encryption_bits   128
-  @iv                :crypto.strong_rand_bytes(16)
+  @encryption_bits 128
+  @iv String.duplicate(<<0>>, 16)
 
-  def pad(data, block_size) do
-    to_add = block_size - rem(byte_size(data), block_size)
-    data <> to_string(:string.chars(to_add, to_add))
-  end
-
-  def unpad(data) do
-    to_remove = :binary.last(data)
-    :binary.part(data, 0, byte_size(data) - to_remove)
-  end
-
-  def genEncode(site_key, api_key, options \\ []) do
-    url_safe = Keyword.get(options, :url_safe, true)
-
-    fn(data) ->
-      formatted_data = data
+  @spec gen_encode!(String.t(), String.t()) :: (String.t() -> String.t())
+  def gen_encode!(site_key, api_key) do
+    fn data ->
+      formatted_data =
+        data
+        |> Jason.encode!()
         |> pad(@aes128_block_size)
 
       :crypto.block_encrypt(:aes_cbc128, generate_key(site_key, api_key), @iv, formatted_data)
+      |> Base.url_encode64()
     end
   end
 
-  def genDecode(site_key, api_key, options \\ []) do
-    url_safe = Keyword.get(options, :url_safe, true)
+  @spec gen_decode!(String.t(), String.t()) :: (String.t() -> map())
+  def gen_decode!(site_key, api_key) do
+    fn data ->
+      formatted_data = Base.url_decode64!(data)
 
-    fn(data) ->
-      :crypto.block_decrypt(:aes_cbc128, generate_key(site_key, api_key), @iv, data)
-        |> unpad()
+      :crypto.block_decrypt(:aes_cbc128, generate_key(site_key, api_key), @iv, formatted_data)
+      |> unpad()
+      |> Jason.decode!()
     end
   end
 
+  @spec generate_key(String.t(), String.t()) :: binary()
   defp generate_key(password, salt) do
     :crypto.hash(:sha, salt <> password) |> truncate_secret()
   end
 
+  @spec truncate_secret(String.t()) :: String.t()
   defp truncate_secret(secret) do
-    << key::size(@encryption_bits), _rest::binary>> = secret
-    << key::size(@encryption_bits) >>
+    <<key::size(@encryption_bits), _rest::binary>> = secret
+    <<key::size(@encryption_bits)>>
   end
 
+  @spec pad(String.t(), pos_integer()) :: String.t()
+  def pad(data, block_size) do
+    to_add = block_size - rem(byte_size(data), block_size)
+    data <> :binary.copy(<<to_add>>, to_add)
+  end
+
+  @spec unpad(binary()) :: binary()
+  def unpad(data) do
+    to_remove = :binary.last(data)
+    :binary.part(data, 0, byte_size(data) - to_remove)
+  end
 end
