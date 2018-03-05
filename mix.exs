bee28786362357cec3d7bb54254e25cadb66c5a6
IRog <ipr210lehigh@gmail.com>
2018-03-05 10:02:03 -0800

    travis coveralls command

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule MultipassEx.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.travis": :test
       ],
       dialyzer: [
         flags: [
