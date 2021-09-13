d55b0b241d5bb12c41b27957337419f498fc1fc8
Luis Ferreira <hidnasio@gmail.com>
2021-09-13 12:53:10 -0300

    Update Elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExMultipass.MixProject do
   def project do
     [
       app: :ex_multipass,
-      version: "0.2.0",
-      elixir: "~> 1.6",
+      version: "0.3.0",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: description(),
